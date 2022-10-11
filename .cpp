d42ebe22bd3b6dc02babb93054358685659704da
ashofhabil <115543295+ashofhabil@users.noreply.github.com>
2022-10-11 17:36:02 +0700

    Update and rename .gitignore to .cpp

---
+++ .cpp
@@ -0,0 +1,32 @@
+// NAMA		:Ashof Habil Pratama
+// NRP		:5020221093
+// Jurusan	:Teknik Kelautan
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+
+tipe_data mencari_V0(tipe_data variabel1, tipe_data variabel2)
+{
+	/* Vo^2.sin.2.alpha/g */
+  	return 
+}
+
+tipe_data speed_dgn_loss(tipe_data variabel)
+{
+	/* Vo=25;alpha=45;g=10 */
+    return
+}
+
+int main() {
+    /* tulis kode utama kalian disini */
+  
+  	/* input adalah kecepatan tangensial maksimum roller */
+  	/* std::cin >> input */
+  
+  	/* std::cout << jarak << " " << kecepatan tangensial << std::endl */
+    return 0;
+}
